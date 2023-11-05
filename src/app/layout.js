f9f1c1400122e13a6f8e78a3be4b4586827436e9
PaulMWolf <145740194+PaulMWolf@users.noreply.github.com>
2023-11-05 11:53:11 -0600

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,11 +2,21 @@ export default function DashboardLayout({
   children, // will be a page or nested layout
 }) {
   return (
+    <html lang = "en">
+    <head>
+      <title>Blog</title>
+    <meta charSet="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </head>
+    
+    <body>
     <section>
       {/* Include shared UI here e.g. a header or sidebar */}
       <nav></nav>
  
       {children}
     </section>
+    </body>
+    </html>
   )
 }
